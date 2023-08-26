293f9de1f4442983f0ef728372061c8d1b771f1c
unknown <danilog1000@gmail.com>
2023-08-26 12:45:11 -0500

    filter button styles added

--- src/components/ButtonFilter/ButtonFilter.tsx
+++ src/components/ButtonFilter/ButtonFilter.tsx
@@ -1,9 +1,18 @@
-import './button.scss';
+import React from 'react';
+import FilterIcon from '../../assets/svg/filter.svg';
+import './buttonFilter.scss';
 
 export default function ButtonFilter() {
 	return (
 		<div className='button-filter'>
-			<button>Filter</button>
+			<button>
+				FILTRAR
+				<img
+					src={FilterIcon}
+					alt=''
+					className=''
+				/>
+			</button>
 		</div>
 	);
 }
